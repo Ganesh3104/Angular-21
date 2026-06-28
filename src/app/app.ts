ad7423a8396fe977a1bca79be5ab953890bb8c7b
Ganesh Ledange <ganeshledange2020@gmail.com>
2026-06-28 19:23:36 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Login } from './login/login';
+import { SignUp } from './sign-up/sign-up';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login, SignUp],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  // template: `<h1>inline tag {{name}}</h1>`,    inline html
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-21');
+  // protected readonly title = signal('angular-21');
+  // name="anil kumar"
+
+  // updateName(){
+  //   this.name="sidh"
+  // }
+
+  // interpolation
+
+  // name="anil kumar"
+  // email="abc@gmail.com"
+
+  // getName(a:number, b:number){
+  //   return a+b;
+  // }
+
+
+
+
+
+
+
 }
